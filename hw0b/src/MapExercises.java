abde2646215e4bed43dd3345e4f1ce0d2a1379eb
blip-cmd <niiakweibrown1@gmail.com>
2025-10-14 12:14:27 +0000

    hw2tsk3: Implement map creation methods: letterToNum, squares, and countWords

--- hw0b/src/MapExercises.java
+++ hw0b/src/MapExercises.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -6,21 +7,39 @@ public class MapExercises {
      * 1, 'b' is 2, 'c' is 3, ..., 'z' is 26.
      */
     public static Map<Character, Integer> letterToNum() {
-        // TODO: Fill in this function.
-        return null;
+        // brute
+//        Map<Character, Integer> map = new HashMap<>();
+//        map.put(1,"a");   map.put(2,"b") ;  map.put(3,"c");
+
+        //programatically. Java uses UNICODE, where a starts at 97
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c = 'a'; c <= 'z'; c++) {
+            map.put(c, c-'a'+1);
+        }
+
+        return map;
     }
 
     /** Returns a map from the integers in the list to their squares. For example, if the input list
      *  is [1, 3, 6, 7], the returned map goes from 1 to 1, 3 to 9, 6 to 36, and 7 to 49.
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
-        // TODO: Fill in this function.
-        return null;
+        Map<Integer, Integer> squared_map = new HashMap<Integer, Integer>();
+
+        for(int value: nums){
+            squared_map.put(value, value * value); // or  (int) Math.pow(value, 2))
+        }
+
+        return squared_map;
     }
 
     /** Returns a map of the counts of all words that appear in a list of words. */
     public static Map<String, Integer> countWords(List<String> words) {
-        // TODO: Fill in this function.
-        return null;
+        Map<String, Integer> wordCounts = new HashMap<>();
+
+        for(String word: words){
+            wordCounts.put(word,wordCounts.getOrDefault(word,0)+1 );
+        }
+        return wordCounts;
     }
 }
